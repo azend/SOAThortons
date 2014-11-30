0250d458acdf478a9368350b2cb4d076966ff463
Verdi R-D <verdi@azend.org>
2014-11-30 17:33:32 -0500

    Added comms class for high level interfacing with services

---
+++ ThortonsHL7/ThortonsHL7/Comms.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThortonsHL7
+{
+    public class Comms
+    {
+        private Dictionary<string, string> registerTeam() {
+            Dictionary<string, string> teamInfo = new Dictionary<string, string>();
+
+            return teamInfo;
+        }
+
+        public async Task<Dictionary<string, string>> GetServices()
+        {
+            Dictionary<string, string> teamInfo = new Dictionary<string, string>();
+
+            return teamInfo;
+        }
+    }
+}
