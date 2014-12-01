554bb604d2ae5880718108742b4a1132519853d3
Verdi R-D <verdi@azend.org>
2014-12-01 13:09:17 -0500

    Created error handler to return a properly formatted HL7 error when something bad happens

---
+++ ThortonService/ThortonService/ErrorHandler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThortonService.IO;
+
+namespace ThortonService
+{
+    public class ErrorHandler
+    {
+        public static string GenerateMessage(int errorCode, string errorMessage)
+        {
+            return string.Format(HL7SpecialChars.BOM + "SOA|NOT-OK|{0}|{1}||" + HL7SpecialChars.EOS + HL7SpecialChars.EOM + HL7SpecialChars.EOS, errorCode, errorMessage);
+        }
+    }
+}
