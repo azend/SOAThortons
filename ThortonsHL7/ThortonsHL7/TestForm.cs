e56a5098eb3372295b4209427678bd1e3c6429f7
MathewCain <goaliesupreme@hotmail.com>
2014-11-30 20:37:48 -0500

    Added some more to testing

--- ThortonsHL7/ThortonsHL7/TestForm.cs
+++ ThortonsHL7/ThortonsHL7/TestForm.cs
@@ -36,14 +36,14 @@ namespace ThortonsHL7
 
         private void button3_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Testing Response: SOA|OK|||9|SRV|TeamFreelancer|totalPurchase||2|5|Returns a calculated amount of money| ARG|1|Province|string|mandatory|| ARG|2|totalMoney|float|mandatory|| RSP|1|PST|float||RSP|2|HST|string||RSP|3|GST|string||RSP|4|SubTotalAmount|string||RSP|5|TotalPurchaseAmount|string||MCH|142.112.50.103|50002|   \nReturn: " + QueryService.ParseMessage("SOA|OK|||9|SRV|TeamFreelancer|totalPurchase||2|5|Returns a calculated amount of money| ARG|1|Province|string|mandatory|| ARG|2|totalMoney|float|mandatory|| RSP|1|PST|float||RSP|2|HST|string||RSP|3|GST|string||RSP|4|SubTotalAmount|string||RSP|5|TotalPurchaseAmount|string||MCH|142.112.50.103|50002| Server"));
-            MessageBox.Show("Testing Response: SOA|NOT-OK|-2|DRC/UNREG-TEAM segment not according to Spec. || \nReturn: " + UnregisterTeam.ParseMessage("SOA|NOT-OK|-2|DRC/UNREG-TEAM segment not according to Spec. ||"));
-            MessageBox.Show("Testing Response(Missing a perameter): SOA|OK|1180||| \nReturn: " + UnregisterTeam.ParseMessage("SOA|OK|1180|||"));            
+            MessageBox.Show("Testing Response: SOA|OK|||9|SRV|TeamFreelancer|totalPurchase||2|5|Returns a calculated amount of money| ARG|1|Province|string|mandatory|| ARG|2|totalMoney|float|mandatory|| RSP|1|PST|string||RSP|2|HST|string||RSP|3|GST|string||RSP|4|SubTotalAmount|string||RSP|5|TotalPurchaseAmount|string||MCH|142.112.50.103|50002|   \nReturn: " + QueryService.ParseMessage("SOA|OK|||9|SRV|TeamFreelancer|totalPurchase||2|5|Returns a calculated amount of money| ARG|1|Province|string|mandatory|| ARG|2|totalMoney|float|mandatory|| RSP|1|PST|string||RSP|2|HST|string||RSP|3|GST|string||RSP|4|SubTotalAmount|string||RSP|5|TotalPurchaseAmount|string||MCH|142.112.50.103|50002| Server" ));
+            MessageBox.Show("Testing Response: SOA|NOT-OK|-1|SOA command <whatever you sent> - UNKNOWN|| \nReturn: " + UnregisterTeam.ParseMessage("SOA|NOT-OK|-1|SOA command <whatever you sent> - UNKNOWN||"));           
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            MessageBox.Show("Testing Response: PUB|OK|||5|RSP|1|PST|string|0>|RSP|2|HST|string|13|RSP|3|GST|string|0|RSP|4|SubTotalAmount|string|100|RSP|5|TotalPurchaseAmount|string|113| \nReturn: " + ExecuteService.ParseMessage("PUB|OK|||5|RSP|1|PST|string|0>|RSP|2|HST|string|13|RSP|3|GST|string|0|RSP|4|SubTotalAmount|string|100|RSP|5|TotalPurchaseAmount|string|113|"));
+            MessageBox.Show("Testing Response: SOA|NOT-OK|-4|No team '<teamName>' (ID : <teamID>) found registered in Dbase|| \nReturn: " + ExecuteService.ParseMessage("SOA|NOT-OK|-4|No team '<teamName>' (ID : <teamID>) found registered in Dbase ||"));  
         }
     }
 }
